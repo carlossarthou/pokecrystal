5bc82024ed43f9ffe27850af67b702f3ad74bc34
carlossarthou <38755745+carlossarthou@users.noreply.github.com>
2019-08-05 22:04:50 +0800

    Revert shiny dex patch (Pt 1 of 3)
    Reference: https://github.com/pret/pokecrystal/wiki/Option-to-show-shiny-colors-in-Pok%C3%A9dex

--- wram.asm
+++ wram.asm
@@ -785,12 +785,13 @@ wDexListingCursorBackup:: db
 wBackupDexListingCursor:: db
 wBackupDexListingPage:: db
 wDexCurLocation:: db
+if DEF(_CRYSTAL11)
 wPokedexStatus:: db
-wPokedexShinyToggle::
-; bit 0: set if displaying shiny palettes
-	db
 wPokedexDataEnd::
-	ds 1
+else
+wPokedexDataEnd:: ds 1
+endc
+	ds 2
 
 NEXTU ; c6d0
 ; pokegear
@@ -1369,7 +1370,12 @@ wCreditsLYOverride:: db
 NEXTU ; cf64
 ; pokedex
 wPrevDexEntryJumptableIndex:: db
+if DEF(_CRYSTAL11)
 wPrevDexEntryBackup:: db
+else	
+wPrevDexEntryBackup::	
+wPokedexStatus:: db	
+endc
 
 NEXTU ; cf64
 ; pokegear
